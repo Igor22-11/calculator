817771e5be838488ad96f774873c114ad6585ac8
Igor Bespalov <igorbespalov89@gmail.com>
2025-04-05 10:16:33 +0300

    add func get data

---
+++ script/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const rootDiv = document.body;
+  getData();
+
+  async function getData() {
+    try {
+      let materialsData;
+      let configData;
+      const dataResponse = await fetch('./data/data.json');
+      if (dataResponse.ok) {
+        materialsData = await dataResponse.json();
+      } else {
+        throw new Error(`Произошла ошибка при загрузке данных data.json: ${dataResponse.status}`);
+      }
+
+      const configResponse = await fetch('./data/config.json');
+      if (configResponse.ok) {
+        configData = await configResponse.json();
+      } else {
+        throw new Error(`Произошла ошибка при загрузке данных config.json: ${configResponse.status}`);
+      }
+
+
+
+      renderCalculator(rootDiv, configData, materialsData);
+
+    } catch (error) {
+      rootDiv.innerHTML = `<div class="error">Произошла ошибка при загрузке данных: ${error.message}</div>`;
+    }
+  }
+
+
+});
+
+
+function renderCalculator(rootDiv, configData, materialsData) {
+  console.log(rootDiv)
+  console.log(configData)
+  console.log(materialsData)
+}
